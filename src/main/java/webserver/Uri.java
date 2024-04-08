3e6a256c038e6843f0b72fc88cf7a41d8eb2bfb0
배정현 <bae1004kin@gmail.com>
2024-04-08 18:21:52 +0900

    feat : Uri 생성 로직 추가 - 생성 방식 변경 - 파싱 로직 추가

--- src/main/java/webserver/Uri.java
+++ src/main/java/webserver/Uri.java
@@ -1,6 +1,11 @@
 package webserver;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public final class Uri {
     private final String path;
@@ -17,5 +22,69 @@ public final class Uri {
 
     public Map<String, String> params() {
         return this.params;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Uri uri = (Uri) o;
+        return Objects.equals(path, uri.path) && Objects.equals(params, uri.params);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(path, params);
+    }
+
+    private final class Parser {
+        private final String uri;
+
+        Parser(String uri) {
+            Objects.requireNonNull(uri);
+            this.uri = uri;
+        }
+
+        void parse() {
+            Uri.this.params = new HashMap<>();
+            String[] chunks = this.uri.split("\\?");
+            if (chunks[0].isBlank()) {
+                chunks[0] = DEFAULT_PATH;
+            }
+            validateAndExtractPath(chunks[0]);
+            validateAndExtractParams(chunks);
+        }
+
+        private void validateAndExtractPath(String path) {
+            Matcher matcher = PATH_PATTERN.matcher(path);
+            if (!matcher.matches()) {
+                throw new IllegalArgumentException("Path 형태가 올바르지 않습니다.");
+            }
+            Uri.this.path = path;
+        }
+
+        private void validateAndExtractParams(String[] chunks) {
+            if (chunks.length > 2) {
+                throw new IllegalArgumentException("쿼리 파라미터의 구분자는 1개만 주어져야 합니다.");
+            }
+            if (chunks.length == 1) {
+                return;
+            }
+            extractParams(chunks);
+        }
+
+        private void extractParams(String[] chunks) {
+            Matcher matcher = Uri.PARAM_PATTERN.matcher(chunks[1]);
+            if (!matcher.matches()) {
+                throw new IllegalArgumentException("쿼리 파라미터 구성이 올바르지 않습니다.");
+            }
+            String[] params = chunks[1].split("&");
+            Arrays.stream(params).forEach(this::putParam);
+        }
+
+        private void putParam(String param) {
+            String[] keyValue = param.split("=");
+            Uri.this.params.put(keyValue[0], keyValue[1]);
+        }
     }
 }
--- src/test/java/webserver/UriTest.java
+++ src/test/java/webserver/UriTest.java
@@ -3,23 +3,16 @@ package webserver;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @DisplayName("Uri 관련 테스트")
 public class UriTest {
     @Test
     void Uri_객체의_필수_인자를_받아서_생성한다() {
-        String path = "/";
-        Map<String, String> params = new HashMap<>() {{
-            put("Host", "localhost:8080");
-            put("Connection", "keep-alive");
-            put("Accept", "*/*");
-        }};
-        Uri uri = new Uri(path, params);
-        assertThat(uri.path()).isEqualTo(path);
-        assertThat(uri.params()).isEqualTo(params);
+        String path = "/?param=param&a=b&c=d";
+        Uri uri = new Uri(path);
+        assertThat(uri).isNotNull();
+        assertThat(uri.params().size()).isEqualTo(3);
     }
 }
